057b20c051fa10b9b4d9ab7e7d37c20111ea433f
fernanda rodriguez <fernandarpy@gmail.com>
2022-08-05 15:26:45 -0500

    actializacion

--- ejercicio13.js
+++ ejercicio13.js
@@ -1,7 +1,20 @@
+
 //Dado una array ,dividirlo en  un numero de subarrays
 //como sea necesario basandonos en un numero que indique
 //su tamaño
-const dividirArray = (array, dividir ) => {
-array.slice
-
-}
+const dividirArray = (array, dividir) => {
+    let arreglos = []
+    array
+    dividir
+    let dividiendo = dividir
+    for (let i = 0; i < array.length ;i += dividiendo ){
+        i
+        let partiendo = array.slice(i, i + dividiendo)
+        partiendo
+        arreglos.push(partiendo)
+        arreglos
+    }
+    return arreglos
+    }
+da = dividirArray([1,2,3,4,5,6,7,8,9,10],3)
+da
