77147443eb88bc57b3e10c9478c144323358e275
fernanda rodriguez <fernandarpy@gmail.com>
2022-08-05 15:25:22 -0500

    ejercicio 12 y 13

---
+++ ejercicio12.js
@@ -0,0 +1,20 @@
+// dado un numero mostrar todos   los numeros desde  ese  al 0 de 8 en 8 
+//en una lista con guiones donde cada numero debe empezar por n
+const listarNumerosHastaCero = (numero) => {
+    let resultado = `---DEL ${numero} al 0 --- \n`
+        for (let i = numero ; i >= 0 ; i--){
+            i
+        if (i % 8 === 0) {
+            i
+            resultado += `-nº ${i}\n`
+        }
+        if(i === 0) {
+         i
+         resultado += "----FIN----"
+         return resultado
+        }
+      }
+    }
+    c = listarNumerosHastaCero(8)
+    c
+    
---
+++ ejercicio13.js
@@ -0,0 +1,7 @@
+//Dado una array ,dividirlo en  un numero de subarrays
+//como sea necesario basandonos en un numero que indique
+//su tamaño
+const dividirArray = (array, dividir ) => {
+array.slice
+
+}
