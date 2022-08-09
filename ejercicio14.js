023030e6764654601c083ce30666863ff4c83fef
fernanda rodriguez <fernandarpy@gmail.com>
2022-08-09 09:33:09 -0500

    ejercicio 14

---
+++ ejercicio14.js
@@ -0,0 +1,11 @@
+const repetirString = (string , numeroVeces) => {
+    let resultado =""
+    for (let i = 0; i <= numeroVeces; i++){
+     resultado += string
+    
+    }
+    return resultado
+    
+    }
+    ps = repetirString ("hola", 3)
+    ps
